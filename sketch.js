b8c1029be656c0b1fcdce272cd863a3d3ee19771
luizotaviodasilva2 <00001095132386sp@al.educacao.sp.gov.br>
2024-09-10 09:29:20 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,12 @@
+setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  function draw() {
+    stroke("blue");
+    fill("red");
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
